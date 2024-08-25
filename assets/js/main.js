f773053b79cdcca08e7a0ba0d3c83a2ff7c51741
Kayum Khan <kayum9721@gmail.com>
2024-08-25 19:01:35 +0530

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,111 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close');
+
+/* Menu Show */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu');
+    })
+}
+
+/* Menu Hidden */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*=============== Add Blur To HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header');
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header');
+}
+window.addEventListener('scroll', blurHeader);
+
+/*=============== Email JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message');
+    
+const sendEmail = (e) => {
+    e.preventDefault();
+    //  serviceID - templateID - #form - publickey
+    emailjs.sendForm('service_bfk3v2i', 'template_ycz3w3h', '#contact-form', 'DDAq4FUHKXFQ7NNeL')
+        .then(() => {
+            // Show send message
+            contactMessage.textContent = 'Message send successfully ✅'
+            
+            // Remove message after five seconds
+            setTimeout(() => {
+                contactMessage.textContent = ''
+            }, 5000)
+            
+            // Clear input fields
+            contactForm.reset()
+            
+        }, () => {
+            // Show error message
+            contactMessage.textContent = 'Message not send ❌'
+        }
+    )
+}
+contactForm.addEventListener('submit', sendEmail);
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll');
+};
+window.addEventListener('scroll', scrollUp);
+ 
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link');
+		}else{
+			sectionsClass.classList.remove('active-link');
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive);
+
+
+// ScrollReveal Animation
+ const sr =ScrollReveal({
+    origin:'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+   reset: true
+ });
+ 
+ sr.reveal(`.home__data, .home__social, .contact__container, .footer__container`);
+ sr.reveal(`.home__image`, {origin: 'bottom'});
+ sr.reveal(`.about__data, .skills__data`, {origin: 'left'});
+ sr.reveal(`.about__image, .skills__content`, {origin: 'right'});
+ sr.reveal(`.education__card, .projects__card`, {interval: 100});
